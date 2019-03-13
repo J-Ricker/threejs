031c43293ecde8d978735b31f6d59ec1434f50a4
Jeremy R <lvlgrinddesign@gmail.com>
2019-03-13 18:27:01 -0400

    working with putting text over threejs

---
+++ js/text.js
@@ -0,0 +1,15 @@
+var loader = new THREE.FontLoader();
+
+loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+
+	var geometry = new THREE.TextGeometry( 'Hello three.js!', {
+		font: font,
+		size: 80,
+		height: 5,
+		curveSegments: 12,
+		bevelEnabled: true,
+		bevelThickness: 10,
+		bevelSize: 8,
+		bevelSegments: 5
+	} );
+} );
